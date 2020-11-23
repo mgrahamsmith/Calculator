ba3f08ea1dc4b02348ee7fd1b961e380156b2612
Graham Smith <mgrahamsmith@protonmail.com>
2020-11-23 03:16:50 -0700

    Added comments and beginnings of parsing input.

--- CalcTests/CalcTests.cs
+++ CalcTests/CalcTests.cs
@@ -6,37 +6,37 @@ namespace CalcTests
     public class CalculatorTests
     {
         [TestMethod]
-        public void Test1()
+        public void TestCalc1()
         {
             TestCalculate("9", "1", "+", "10");
         }
 
         [TestMethod]
-        public void Test2()
+        public void TestCalc2()
         {
             TestCalculate("4", "-7", "-", "11");
         }
 
         [TestMethod]
-        public void Test3()
+        public void TestCalc3()
         {
             TestCalculate("8", "8", "*", "64");
         }
 
         [TestMethod]
-        public void Test4()
+        public void TestCalc4()
         {
             TestCalculate("256", "0", "/", "ERROR: DIVIDE_BY_ZERO");
         }
 
         [TestMethod]
-        public void Test5()
+        public void TestCalc5()
         {
             TestCalculate("", "423", "+", "ERROR: INVALID_INPUT");
         }
 
         [TestMethod]
-        public void Test6()
+        public void TestCalc6()
         {
             TestCalculate("foo", "14", "+", "ERROR: INVALID_INPUT");
         }
@@ -45,6 +45,21 @@ namespace CalcTests
         public void TestOverflow()
         {
             TestCalculate("102423452345234523452345234", "11234524635342452345234523452345", "+", "ERROR: OVERFLOW");
+        }
+
+        [TestMethod]
+        public void TestInputParse()
+        {
+            // Arrange
+            CalculatorNS.Calculator calc = new CalculatorNS.Calculator();
+
+            // Action
+            string inputStr = "3+1+4/6*6*8/3-4*4+7";
+            string actual = calc.parseInput(inputStr);
+
+            // Assert
+            string expected = "3 1 + 4 6 / 6 * 8 * 3 / + 4 4 * - 7 +";
+            Assert.AreEqual(expected, actual, "FAIL: Failed to parse expression.");
         }
 
         public void TestCalculate(string leftVal, string rightVal, string operatorStr, string expected)
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CalculatorNS
 {
@@ -13,6 +14,7 @@ namespace CalculatorNS
             DIVIDE_BY_ZERO,
             INVALID_INPUT,
             OVERFLOW,
+            PARSE_ERROR,
             UNKOWN
         }
 
@@ -22,6 +24,7 @@ namespace CalculatorNS
         private int m_result;
         private Status m_status = Status.GOOD;
 
+        // Class and Methods for Calculator Project Part 1 /////////////////////
         public Calculator()
         {
             setLeft("0");
@@ -48,14 +51,16 @@ namespace CalculatorNS
 
         public void setOperator(string operator_str)
         {
-            if (validOperator(operator_str))
+            if (isOperator(operator_str))
             {
                 m_operator = operator_str;
             }
-        }
-        public string parseInput(string inputStr)
-        {
-            return inputStr;
+            else
+            {
+                Console.Write("Invalid operator: " + operator_str + ".  Expected: +,-,*,/\n");
+                m_status = Status.INVALID_INPUT;
+            }
+
         }
 
         public int calculate()
@@ -119,10 +124,77 @@ namespace CalculatorNS
             return result;
         }
 
-        private bool validOperator(string operator_str)
+        public string getPreviousResult(int index)
+        {
+            int min = 0;
+            int max = 10;
+            if (index < min || index > max)
+            {
+                string errMsg = $"ERROR: Invalid index.  Must be a value in the range {min}-{max}";
+                Console.Write(errMsg);
+                throw new System.IndexOutOfRangeException(errMsg);
+            }
+
+            throw new System.NotImplementedException("Implementation not completed.");
+        }
+
+
+        // Methods for Calculator Project Part 2 ///////////////////////////////
+        public string parseInput(string inputStr)
+        { 
+            string parsedResult = "";
+
+            char[] parsedChars = inputStr.ToCharArray();
+            Queue<char> operatorChars = new Queue<char>();  // Queue for operators
+            Queue<int> numbers = new Queue<int>();          // Queue for Int32 operands
+
+            for (int i=0; i<parsedChars.Length; i++)
+            {
+                string numStr = "";
+                char tmpChar = parsedChars[i];
+
+                int opPrec = getPrecedence(tmpChar);
+
+                if (opPrec != -1)
+                {
+                    // If we reached an operator, check numStr to see if we
+                    // need to enqueu on the number Queue.
+                    if (!String.Equals(numStr, ""))
+                    {
+                        numbers.Enqueue(strToInt(numStr));
+                        // Reset numStr
+                        numStr = "";
+                    }
+
+                    if (opPrec > getPrecedence(operatorChars.Peek()))
+                    {
+
+                    }
+                    operatorChars.Enqueue(tmpChar);
+                }
+                else if (System.Char.IsDigit(tmpChar))
+                {
+                    numStr += tmpChar;
+                }
+                else
+                {
+                    string errMsg = $"ERROR: Invalid input string.  Expected expression with no spaces" +
+                        $", including only 0-9,+,-,*, or /.  Ex: '10-2*6/4'";
+                    Console.Write(errMsg);
+                    m_status = Status.PARSE_ERROR;
+                }
+                
+            }
+
+            return parsedResult;
+        }
+
+        // Helper methods //////////////////////////////////////////////////////
+
+        // Return true if operator is valid ("+", "-", "*", "/")
+        private bool isOperator(string operator_str)
         {
             string[] expectedOps = { "+", "-", "*", "/" };
-
             for (int i = 0; i < expectedOps.Length; i += 1)
             {
                 if (String.Equals(operator_str, expectedOps[i]))
@@ -130,12 +202,35 @@ namespace CalculatorNS
                     return true;
                 }
             }
-
-            Console.Write("Invalid operator: " + operator_str + ".  Expected: +,-,*,/\n");
-            m_status = Status.INVALID_INPUT;
             return false;
         }
 
+        // Return true if operator is valid ('+', '-', '*', '/')
+        // Returns -1 if the operator is not valid
+        private int getPrecedence(char operator_str)
+        {
+            int precedence = -1;
+            if (String.Equals(operator_str, '+'))
+            {
+                precedence = 0;
+            }
+            else if (String.Equals(operator_str, '-'))
+            {
+                precedence = 0;
+            }
+            else if (String.Equals(operator_str, '*'))
+            {
+                precedence = 1;
+            }
+            else if (String.Equals(operator_str, '/'))
+            {
+                precedence = 1;
+            }
+
+            return precedence;
+        }
+
+        // Converts string input to Int32
         private int strToInt(string inpStr)
         {
             int result = -1;
@@ -158,6 +253,7 @@ namespace CalculatorNS
             return result;
         }
 
+        // Add two Int32 values
         private int add(int val1, int val2)
         {
             int result = -1;
@@ -176,11 +272,13 @@ namespace CalculatorNS
             return result;
         }
 
+        // Subtract two Int32 values
         private int subtract(int val1, int val2)
         {
             return val1 - val2;
         }
 
+        // Divide two Int32 values
         private int divide(int val1, int val2)
         {
             if (val2 == 0)
@@ -192,14 +290,24 @@ namespace CalculatorNS
             return val1 / val2;
         }
 
+        // Multipy two Int32 values
         private int multiply(int val1, int val2)
         {
             return val1 * val2;
         }
 
+        // 
+
+        // Main
         public static void Main()
         {
+            Console.Write("Time to calculate.");
+            // Arrange
             Calculator calc = new Calculator();
+
+            // Action
+            string inputStr = "3+1+4/6*6*8/3-4*4+7";
+            string actual = calc.parseInput(inputStr);
         }
     }
 }
